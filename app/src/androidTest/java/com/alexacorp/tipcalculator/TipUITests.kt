cbc9dc0153a0f93ae328bf5f4860ef09c934690a
alexander.laputko <alexander.laputko@ya.ru>
2022-10-08 14:27:58 +0300

    Add local test and instrumentation tests.

---
+++ app/src/androidTest/java/com/alexacorp/tipcalculator/TipUITests.kt
@@ -0,0 +1,26 @@
+package com.alexacorp.tipcalculator
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import com.alexacorp.tipcalculator.ui.theme.TipCalculatorTheme
+import org.junit.Rule
+import org.junit.Test
+
+class TipUITests {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculate_20_percent_tip() {
+        composeTestRule.setContent {
+            TipCalculatorTheme {
+                TipTimeScreen()
+            }
+        }
+        composeTestRule.onNodeWithText("Bill Amount").performTextInput("10")
+        composeTestRule.onNodeWithText("Tip (%)").performTextInput("20")
+        composeTestRule.onNodeWithText("Tip amount: 2,00 ₽").assertExists()
+    }
+}
